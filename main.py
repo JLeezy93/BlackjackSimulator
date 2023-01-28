d768c650a5a22fdedc796f66b46e67e835e0e9e4
Josh <lee.josh93@gmail.com>
2023-01-28 15:25:21 -0600

    worked on card values

--- main.py
+++ main.py
@@ -23,18 +23,21 @@ face_cards = [
         'KH', 'KD', 'KS', 'KC',
         'QH', 'QD', 'QS', 'QC',
         'JH', 'JD', 'JS', 'JC']
-numeric_cards = [
-        '10H', '10D', '10S', '10C',
-        '9H', '9D', '9S', '9C',
-        '8H', '8D', '8S', '8C',
-        '7H', '7D', '7S', '7C',
-        '6H', '6D', '6S', '6C',
-        '5H', '5D', '5S', '5C',
-        '4H', '4D', '4S', '4C',
-        '3H', '3D', '3S', '3C',
-        '2H', '2D', '2S', '2C']
+#numeric_card_values
+ten_val = ['10H', '10D', '10S', '10C']
+nine_val = ['9H', '9D', '9S', '9C']
+eight_val = ['8H', '8D', '8S', '8C']
+seven_val = ['7H', '7D', '7S', '7C']
+six_val = ['6H', '6D', '6S', '6C']
+five_val = ['5H', '5D', '5S', '5C]
+four_val = ['4H', '4D', '4S', '4C']
+three_val = ['3H', '3D', '3S', '3C']
+two_val = ['2H', '2D', '2S', '2C']
+
 
 # Deal hand
+player_hand_value = 0
+player_hand_total_value = 0
 player_hand = random.choices(deck, k=2)
 
 
@@ -82,15 +85,39 @@ def blackjack_odds(hand):
     # Return the odds
     return odds
 
-def blackjack_or_bust():
-    for cards_in_hand in player_hand:
-        for card in deck:
-            if card == cards_in_hand:
+# def blackjack_or_bust():
+#     for cards_in_hand in player_hand:
+#         for card in deck:
+#             if card == cards_in_hand:
+
 
 
 def assign_card_values(cards_in_hand):
     for cards_in_hand in player_hand:
-        if card == aces
+        if card == aces:
+            player_hand_value += 11
+        elif card == face_cards:
+            player_hand_value += 10
+        elif card == ten_val:
+            player_hand_value += 10
+        elif card == nine_val:
+            player_hand_value += 9
+        elif card == eight_val:
+            player_hand_value += 8
+        elif card == seven_val:
+            player_hand_value += 7
+        elif card == six_val:
+            player_hand_value += 6
+        elif card == five_val:
+            player_hand_value += 5
+        elif card == four_val:
+            player_hand_value += 4
+        elif card == three_val:
+            player_hand_value += 3
+        elif card == two_val:
+            player_hand_value += 2
+    print(player_hand_value)
+
 
 
 
@@ -100,4 +127,5 @@ def assign_card_values(cards_in_hand):
 print(player_hand)
 print('Cards in deck: ' + str(len(deck)))
 remove_cards_dealt()
+assign_card_values()
 hit_or_stay()
